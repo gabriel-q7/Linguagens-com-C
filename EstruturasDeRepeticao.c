f32fe3b31b259fd7b98c2498b30c82fca4fce45c
Gabriel Aguiar Aquino <gabriel_aq7@hotmail.com>
2020-05-24 11:08:23 -0300

    Exemplos de estruturas de repetição na linguagem C

---
+++ EstruturasDeRepeticao.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+    Estruturas de repetição 24/05/20 
+******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int contador,i,numero; 
+      
+    /*while(contador>-10){
+    printf("%d,",contador);
+    contador--;
+    }
+    */
+    
+    /*for(i=0;i<10;i++){
+        printf("%d,",i);
+    }
+    */
+    
+    /*do{
+        printf("\nDigite um numero(0 para sair):");
+        scanf("%d",&numero);
+        printf("Seu numero é %d",numero);
+    }while(numero!=0);
+    */
+    
+    return 0;
+}
